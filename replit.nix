1b32f0d7bc93118c8103e8a4ba39bc90763bb7b0
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-02-05 03:47:32 +0000

    fix replit.nix

--- replit.nix
+++ replit.nix
@@ -1,5 +1,11 @@
 { pkgs }: {
     deps = [
-        pkgs.cowsay
+      pkgs.deno
+      pkgs.rustup
+      pkgs.rustc
+      pkgs.rustfmt
+      pkgs.cargo
+      pkgs.cargo-edit
+      pkgs.rust-analyzer
     ];
 }
